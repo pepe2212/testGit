36dac4f2ae2d0a076a67717e36bfef8612a389b0
Jose Arana <josie2604@hotmail.com>
2021-04-21 19:51:02 -0500

    New Script

--- FillNumbers/src/FillNumbers.java
+++ FillNumbers/src/FillNumbers.java
@@ -1,12 +1,20 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Test
-{  
+{ 
+	/*Class Name:Test
+	 Purpose:This class will allow to return an order array based on a given unordered array as input
+	 By:Jose Arana*/
     public static int maxValue=0;
+   
+    /*This is the void called by the main void which will call the private method findMaxNumber,
+     which will return the max number in the array*/
     public static int[] returnNumbers(int[] a){
-       if (a.length<0){
-        System.out.println("El array esta sin valores!");
+  
+    	if (a.length<0){
+    		System.out.println("El array esta sin valores!");
         }
+	
         int maxValue=findMaxNumber(a);
         int[]array = new int[maxValue];
        
@@ -17,15 +25,16 @@ class Test
     }
     
     private static int findMaxNumber(int[]a ){
-      if (a.length>0){
-        maxValue=a[0];
+      
+    	if (a.length>0){
+    		maxValue=a[0];
           for (int j : a) {
               if (j > maxValue) {
                   maxValue = j;
               }
           }
       }else{
-        System.out.println("El array esta sin valores");
+    	  	System.out.println("El array esta sin valores!");
         }  
     return maxValue;
        }
@@ -33,12 +42,16 @@ class Test
     
     public static void main(String[] args)
     {
+    	/*This is the input to test the code*/
       int []a={58,60,65};
-        if(a.length>0){
+        
+      if(a.length>0){
            System.out.println(Arrays.toString(returnNumbers(a)));
         }else {
-        System.out.println("El array esta sin valores!");
+        	System.out.println("El array esta sin valores!");
         }
     
+    }
+}
     }
 }
--- ReturnNextPlace/src/ReturnNextPlace.java
+++ ReturnNextPlace/src/ReturnNextPlace.java
@@ -1,19 +1,26 @@
-
-
 class Test
 {  
+	/*Class Name:Test
+	 Purpose: This Java Class will allow 
+     replace a given String with a new String composed by the next letter of each Character
+     By:Jose Arana */
     
     public static final Character[] alphabet ={'a','b','c','d','e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-       
+    
+    /*This is the public method called by the void main, 
+     which will call the private methods findNextCharacter and 
+     replaceCharacter*/   
     public static String returnNextPlace(String text){
-        Character tempCharacter;
-        StringBuilder finalText= new StringBuilder();
+    
+    Character tempCharacter;
+    StringBuilder finalText= new StringBuilder();
+        
         if(text.length() > 0){
         for(int i = 0; i<text.length(); i++){
         Character lastItem= text.charAt(i);
             if(findNextCharacter(lastItem)){
                 tempCharacter=replaceCharacter(lastItem);
-            finalText.append(tempCharacter);
+                finalText.append(tempCharacter);
             }else {
            finalText.append(lastItem);
             }
@@ -21,8 +28,8 @@ class Test
         }else
         {
         System.out.println("El array esta sin valores!");
-        
         }
+        
     return finalText.toString();
     }
     
@@ -37,7 +44,7 @@ class Test
                  newCharacter=alphabet[0];
                 }
             }
-        
+       
         }
     
     return newCharacter;
@@ -51,12 +58,12 @@ class Test
             }
         }
     return false;
-    
     }
     
     
     public static void main(String[] args)
     {
+    	/*This is the main void where the code can be tested*/
         String text="123 abcd*3";
         String sample=returnNextPlace(text);  
         System.out.println(sample);
